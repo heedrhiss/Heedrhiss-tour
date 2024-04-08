902e4dcffc2474cdd34d261e2e4ee8ebdd029e7a
heedrhiss <muyideenidris77@gmail.com>
2024-04-08 16:06:22 +0100

    initial com

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+  const x = 23;
+
+  return (
+      <div>
+        Hello world
+      </div>
+     
+  )
+}
+
+export default App
