c6d4e482f2be015742e52319a9aac2c75ad5fe61
heedrhiss <muyideenidris77@gmail.com>
2024-07-05 18:35:22 +0100

    geolaction

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,25 +1,38 @@
 import { useNavigate, useSearchParams } from 'react-router-dom'
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
-import styles from './Map.module.css'
+import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvents } from 'react-leaflet'
 import { useCities } from '../contexts/CitiesContext'
 import { useEffect, useState } from 'react'
+import styles from './Map.module.css'
+
+
+import { useGeolocation } from '../contexts/GeoLocation'
+import Button from './Button';
 
 function Map() {
-    const navigate = useNavigate()
+    
     const {cities} = useCities()
+    const {position: geoPosition, isLoading: geoIsLoading, getPosition} = useGeolocation()
+
     const [searchParams] = useSearchParams()
     const lat = searchParams.get('lat')
     const lng = searchParams.get("lng")
     const [mapPosition, setMapPosition] = useState([40, 0])
-    // const position = [40,0];
+    
     useEffect(function(){
-        if (lat && lng) setMapPosition([lat, lng])}
-        ,[lat,lng])
-
-        // console.log(lat,lng)
+        if (lat && lng) setMapPosition([lat, lng]);
+        if (geoPosition) setMapPosition(geoPosition);
+    
+    }
+        ,[geoPosition,lat,lng])
+        
     return (
-    <div className={styles.mapContainer} onClick={()=>navigate('form')}>
-        <MapContainer center={mapPosition} zoom={9} scrollWheelZoom={true} className={styles.map}>
+    <div className={styles.mapContainer}>
+        {!geoPosition && 
+            <Button type='position' onclick={getPosition}>
+            {geoIsLoading ? 'Loading...!' : 'Get Location'}
+        </Button>}
+
+        <MapContainer center={mapPosition} zoom={7} scrollWheelZoom={true} className={styles.map}>
         <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"/>
@@ -29,6 +42,7 @@ function Map() {
         </Popup>
         </Marker>)}
         <ChangePosition position={mapPosition}/>
+        <MapClickEvent/>
         </MapContainer>
     </div>
     )
@@ -41,4 +55,19 @@ function ChangePosition({position}){
 
     return null;
 }
+
+function MapClickEvent(){
+
+    const navigate = useNavigate()
+    // const {position: geoPosition} = useGeolocation()
+    
+    useMapEvents({
+        
+        click: e => navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`)
+    
+        
+    })
+    
+}
+
 export default Map
